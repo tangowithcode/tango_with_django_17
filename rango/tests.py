82ca9ba83a9f39e44ec0b38e20fe9738153715e2
leifos <leifos@acm.org>
2014-10-27 14:40:47 +0000

    added some tests

--- rango/tests.py
+++ rango/tests.py
@@ -1,9 +1,19 @@
 from django.test import TestCase
 
+from django.core.urlresolvers import reverse
+
 from rango.models import Category
 
-# Create your tests here.
 
+
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
+
+# Create your tests here.
 
 class CategoryMethodTests(TestCase):
 
@@ -21,4 +31,39 @@ class CategoryMethodTests(TestCase):
         cat.save()
 
         self.assertEqual(cat.slug, 'random-category-string')
+
+
+
+
+
+
+class IndexViewTests(TestCase):
+
+    def test_index_view_with_no_categories(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no categories present.")
+        self.assertQuerysetEqual(response.context['categories'], [])
+
+    def test_index_view_with_categories(self):
+        """
+        Check to make sure that 4 categories are being passed through.
+        """
+
+        add_cat('test',1,1)
+        add_cat('temp',1,1)
+        add_cat('tmp',1,1)
+        add_cat('tmp test temp',1,1)
+
+
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "tmp test temp")
+
+        num_cats =len(response.context['categories'])
+
+        self.assertEqual(num_cats , 4)
 
